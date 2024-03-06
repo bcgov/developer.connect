008fe9b6e3dad34fc52c82c6e0ca310bb4417641
deetz99 <dietrich@wolpert.ca>
2024-03-06 15:40:18 -0800

    create initial card layout

--- web/site/components/Sbc/ProductCard.vue
+++ web/site/components/Sbc/ProductCard.vue
@@ -1,28 +1,38 @@
 <script setup lang="ts">
+const localePath = useLocalePath()
+defineProps<{
+  name: string
+  description: string
+  badge?: string
+  bulletPoints?: string[]
+  directory: string
+}>()
 </script>
 <template>
   <div class="flex h-[420px] w-[390px] flex-col overflow-hidden rounded bg-white shadow-md">
     <div class="relative flex h-[60px] items-center bg-bcGovBlue-350 px-7 py-3.5 font-semibold tracking-wide">
-      <span class="text-white">Business Registry</span>
-      <span class="absolute right-7 top-0 rounded-b bg-bcGovColor-navDivider px-2 py-1 text-sm text-bcGovBlue-600">NEW</span>
+      <a
+        :href="localePath(`/products/${directory}/overview`)"
+        class="text-white focus:outline-none"
+      >
+        {{ name }}
+      </a>
+      <span v-if="badge" class="absolute right-7 top-0 rounded-b bg-bcGovColor-navDivider px-2 py-1 text-sm text-bcGovBlue-600">{{ badge }}</span>
     </div>
     <div class="grow self-start px-7 pt-7 text-left text-bcGovColor-midGray">
       <p class="mb-2">
-        Start a B.C. based business and keep business records up to date. Here you are currently able to:
+        {{ description }}
       </p>
-      <ul class="list-[square] pl-4 font-semibold marker:text-bcGovGray-400">
-        <li>
-          Incorporate and manage benefit companies and cooperative associations.
-        </li>
-        <li>
-          Register and manage general partnerships and sole proprietorships (including "Doing Business As" / DBA names).
+      <ul v-if="bulletPoints" class="list-[square] pl-4 font-semibold marker:text-bcGovGray-400">
+        <li v-for="item in bulletPoints" :key="item">
+          {{ item }}
         </li>
       </ul>
     </div>
     <div class="px-7 pb-7 text-left">
       <span class="flex flex-wrap items-center font-semibold text-[#1a5a96] underline">
         <span>
-          Go to Manufactured Home Registry
+          Go to {{ name }}
           <span class="inline-flex align-middle">
             <UIcon name="i-mdi-arrow-right-thin scale-150 ml-2" />
           </span>
--- web/site/pages/products/index.vue
+++ web/site/pages/products/index.vue
@@ -1,18 +1,42 @@
 <script setup lang="ts">
-// const localePath = useLocalePath()
-const { t } = useI18n()
+const { t, locale } = useI18n()
 useHead({
   title: t('page.products.title')
 })
 definePageMeta({
   layout: 'home'
 })
+
+const { data: products } = await useAsyncData(
+  'product-cards',
+  () => {
+    return queryContent()
+      .where({ _locale: locale.value, _extension: { $eq: 'yml' } })
+      .sort({ _dir: 1 })
+      .find()
+  },
+  {
+    watch: [locale]
+  }
+)
+
+// console.log(products.value)
 </script>
 <template>
-  <div class="mx-auto flex flex-col items-center space-y-2 text-center">
-    <h1 class="text-3xl font-semibold text-bcGovColor-darkGray dark:text-white">
+  <div class="mx-auto w-full max-w-[1360px] p-8">
+    <h1 class="mb-8 text-3xl font-semibold text-bcGovColor-darkGray dark:text-white">
       {{ $t('page.products.h1') }}
     </h1>
-    <SbcProductCard />
+    <ul class="mx-auto flex flex-wrap justify-center gap-8">
+      <SbcProductCard
+        v-for="product in products"
+        :key="product._dir"
+        :name="product.name"
+        :badge="product.badge"
+        :description="product.description"
+        :bullet-points="product.bulletPoints"
+        :directory="product._dir"
+      />
+    </ul>
   </div>
 </template>
