fc2ae0e1c03b15661e749b550d8632b97c255b67
deetz99 <dietrich@wolpert.ca>
2024-03-07 11:04:46 -0800

    create pages/sbc/dashboard file

--- web/site/nuxt.config.ts
+++ web/site/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  ssr: false,
   modules: ['@nuxtjs/eslint-module'],
   extends: ['@daxiom/sbc-nuxt-assets-layer'],
   imports: {
