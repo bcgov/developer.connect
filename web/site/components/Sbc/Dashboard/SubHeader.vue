85dfab4c2a0834d396f3fac87fea526c84f8485d
deetz99 <dietrich@wolpert.ca>
2024-03-12 16:09:05 -0700

    fix mobile styling for dashboard sub header

--- web/site/components/Sbc/Dashboard/SubHeader.vue
+++ web/site/components/Sbc/Dashboard/SubHeader.vue
@@ -3,33 +3,21 @@ const sandboxMode = ref(false)
 </script>
 <template>
   <div
-    class="h-10 w-full border-b p-2 sm:px-4"
+    class="w-full border-b p-2 sm:px-4"
     :class="sandboxMode ? 'bg-bcGovColor-navDivider dark:border-bcGovColor-navDivider' : 'bg-white dark:bg-bcGovColor-darkGray border-bcGovGray-500'"
   >
-    <div class="relative m-auto flex h-full max-w-[1360px] items-center justify-between">
-      <!-- <UButton
-          ref="ignoreElRef"
-          :padded="false"
-          :aria-label="sideNavOpen ? 'Close Navigation' : 'Open Navigation'"
-          aria-haspopup="true"
-          :aria-expanded="sideNavOpen"
-          variant="link"
-          color="gray"
-          icon="i-mdi-menu"
-          class="ml-2 scale-150"
-          @click="sideNavOpen = !sideNavOpen"
-        /> -->
+    <div class="relative m-auto flex h-full max-w-[1360px] flex-col items-center justify-between gap-2 sm:flex-row">
       <span
         v-if="sandboxMode"
-        class="absolute left-1/2 -translate-x-1/2 text-bcGovColor-darkGray"
+        class="flex items-center gap-1 text-bcGovColor-darkGray lg:absolute lg:left-1/2 lg:-translate-x-1/2"
       >
         <span class="inline-flex pb-0.5 align-middle">
           <UIcon name="i-mdi-information-slab-circle scale-150 mr-2" />
         </span>
-        {{ $t('page.dashboard.sandboxMode.infoText') }}
+        <span>{{ $t('page.dashboard.sandboxMode.infoText') }}</span>
       </span>
       <span
-        class="ml-auto font-semibold"
+        class="ml-auto whitespace-nowrap font-semibold"
         :class="sandboxMode ? 'text-bcGovColor-darkGray' : 'dark:text-white'"
       >
         {{ $t('page.dashboard.sandboxMode.text') }}
