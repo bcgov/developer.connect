c816e516efc1c27fadcc1ad3486349fa79857106
deetz99 <dietrich@wolpert.ca>
2024-03-12 16:28:14 -0700

    set ssr back to true

--- web/site/nuxt.config.ts
+++ web/site/nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  ssr: false,
   modules: ['@nuxtjs/eslint-module'],
   extends: ['@daxiom/sbc-nuxt-assets-layer'],
   imports: {
