1dcaca2cefb10f60c21c88d0ca3e7459fccbed35
deetz99 <dietrich@wolpert.ca>
2024-03-12 13:01:32 -0700

    refactor layout/docs

--- web/site/layouts/docs.vue
+++ web/site/layouts/docs.vue
@@ -1,22 +1,6 @@
 <script setup lang="ts">
-const { width } = useWindowSize()
-const sideNavOpen = ref(false)
-watch(
-  width,
-  () => {
-    sideNavOpen.value = width.value >= 1024
-  },
-  { immediate: true }
-)
-
-const sideNavRef = ref(null)
-const ignoreElRef = ref(null)
-
-onClickOutside(sideNavRef, () => {
-  if (sideNavOpen && width.value < 1024) {
-    sideNavOpen.value = false
-  }
-}, { ignore: [ignoreElRef] })
+import type { NavItem } from '@nuxt/content/dist/runtime/types'
+const navigation = inject<Ref<NavItem[]>>('navKey')
 </script>
 
 <template>
@@ -24,31 +8,12 @@ onClickOutside(sideNavRef, () => {
     class="flex min-h-screen flex-col bg-bcGovColor-gray1 dark:bg-bcGovGray-900"
   >
     <SbcHeaderMain />
-    <div
-      v-if="width < 1024"
-      class="h-10 w-full border-b border-bcGovGray-500 bg-white p-2 sm:px-4 dark:bg-bcGovGray-900"
-    >
-      <div class="m-auto flex h-full max-w-[1360px] items-center">
-        <UButton
-          ref="ignoreElRef"
-          :padded="false"
-          :aria-label="sideNavOpen ? 'Close Navigation' : 'Open Navigation'"
-          aria-haspopup="true"
-          :aria-expanded="sideNavOpen"
-          variant="link"
-          color="gray"
-          icon="i-mdi-menu"
-          class="ml-2 scale-150"
-          @click="sideNavOpen = !sideNavOpen"
-        />
-      </div>
-    </div>
     <main class="relative mx-auto flex w-full max-w-[1360px] grow flex-col lg:grid lg:grid-cols-12">
-      <SbcSideNavigation
-        ref="sideNavRef"
-        class="absolute h-full -translate-x-full transition-transform duration-200 ease-in lg:static lg:col-start-1 lg:col-end-3"
-        :class="sideNavOpen ? 'translate-x-0' : '-translate-x-full'"
-      />
+      <aside class="absolute hidden h-full min-w-fit flex-1 overflow-y-auto border-r border-bcGovGray-500 bg-bcGovColor-gray1 px-2 py-4 lg:static lg:col-start-1 lg:col-end-3 lg:block dark:bg-bcGovGray-900">
+        <nav>
+          <SbcAccordianNavigation :nav-items="createContentNav(navigation)" />
+        </nav>
+      </aside>
       <div class="w-full lg:col-span-full lg:col-start-4 xl:-ml-10">
         <slot />
       </div>
