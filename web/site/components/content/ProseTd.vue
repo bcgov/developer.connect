c4e38332e25c725869238759cdac92868e2ae478
deetz99 <dietrich@wolpert.ca>
2024-03-19 11:00:45 -0700

    fix prosetable dark mode colours

--- web/site/components/content/ProseTd.vue
+++ web/site/components/content/ProseTd.vue
@@ -1,5 +1,5 @@
 <template>
-  <td class="border border-bcGovGray-100 p-4 align-middle">
+  <td class="border border-bcGovGray-100 p-4 align-middle dark:border-gray-300/50">
     <slot />
   </td>
 </template>
