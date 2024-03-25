f7ad98b28eeb20c4189efdf594a77d5f34b22bdd
deetz99 <dietrich@wolpert.ca>
2024-03-25 15:42:32 -0700

    add composable for toc active states

---
+++ web/site/composables/headingObserver.ts
@@ -0,0 +1,49 @@
+// intersection observer to apply active class to table of contents component
+export function useHeadingObserver () {
+  // table of contents intersection observer refs
+  const activeTocId = ref<string | null>(null)
+  const observer = shallowRef<IntersectionObserver | null | undefined>(null)
+  const { locale } = useI18n()
+  const routeWithoutLocale = useRouteWithoutLocale()
+
+  const observerOptions = {
+    root: document.getElementById('nuxt-content-wrapper'),
+    threshold: 1,
+    rootMargin: '-100px 0px'
+  }
+
+  // only working with setTimeout, ContentDoc fetch need to complete first?
+  function observeHeadings () {
+    setTimeout(() => {
+      document.querySelectorAll('h2, h3').forEach((section) => {
+        observer.value?.observe(section)
+      })
+    }, 100)
+  }
+
+  // create observer when doc layout mounts
+  onMounted(() => {
+    observer.value = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const id = entry.target.getAttribute('id')
+        if (entry.isIntersecting) {
+          activeTocId.value = id
+        }
+      })
+    }, observerOptions)
+
+    observeHeadings()
+  })
+
+  // remove observer on unmount
+  onUnmounted(() => {
+    observer.value?.disconnect()
+  })
+
+  // observe new headings when route or locale changes
+  watch([locale, routeWithoutLocale], () => {
+    observeHeadings()
+  })
+
+  return { activeTocId }
+}
