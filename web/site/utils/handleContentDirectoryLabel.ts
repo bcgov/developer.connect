e938bb3494eb7ebc278d86c702ad2e63d2fa7c4d
deetz99 <dietrich@wolpert.ca>
2024-04-02 16:12:47 -0700

    test handleContentDirectory util and add fallback string

--- web/site/utils/handleContentDirectoryLabel.ts
+++ web/site/utils/handleContentDirectoryLabel.ts
@@ -1,10 +1,14 @@
 // returns full string of product name from product/folder title
 export function handleContentDirectoryLabel (title: string) {
   const { t } = useI18n()
+  let result: string
+
   if (title === 'Get Started' || title === 'get-started') { // required to return correct fr translation for 'Get Started' accordian title
-    return t('btn.getStarted')
+    result =  t('btn.getStarted')
   } else {
     const key = title.toUpperCase() as keyof typeof ProductNames
-    return ProductNames[key] // return full string from enum, eg: content file 'bn' returns 'Business Number'
+    result = ProductNames[key] // return full string from enum, eg: content file 'bn' returns 'Business Number'
   }
+
+  return result === undefined ? title : result
 }
