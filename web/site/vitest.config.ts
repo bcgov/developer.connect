621e126c99c8a0501b1526508a56c973ca8db71c
deetz99 <dietrich@wolpert.ca>
2024-04-02 14:10:38 -0700

    add test deps

---
+++ web/site/vitest.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath } from 'node:url'
+import { defineVitestConfig } from '@nuxt/test-utils/config'
+
+export default defineVitestConfig({
+  test: {
+    environment: 'nuxt',
+    dir: 'tests',
+    // coverage: {
+    //   reportsDirectory: 'coverage',
+    // },
+    includeSource: ['../pages/index.vue'],
+    environmentOptions: {
+      nuxt: {
+        rootDir: fileURLToPath(new URL('./', import.meta.url)),
+        domEnvironment: 'happy-dom',
+        // mock: {
+        //   indexedDb: true,
+        // },
+      },
+    },
+    // setupFiles: './tests/setup/mocks.ts',
+    globals: true,
+  },
+})
