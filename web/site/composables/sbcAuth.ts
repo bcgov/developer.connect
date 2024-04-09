713ab00732ff7e277ec3790a171df1c11d7a5896
deetz99 <dietrich@wolpert.ca>
2024-04-09 13:33:43 -0700

    composable to handle signout

---
+++ web/site/composables/sbcAuth.ts
@@ -0,0 +1,24 @@
+import {
+  signOut
+} from 'firebase/auth'
+
+// handle auth signout/sign in
+export function useSbcAuth () {
+  const auth = useFirebaseAuth()!
+  const localePath = useLocalePath()
+  // might need these still
+  // const user = useCurrentUser()
+  // const { t } = useI18n()
+  // const router = useRouter()
+  // const route = useRoute()
+
+  // logout user and navigate to homepage
+  function handleUserLogout () {
+    signOut(auth)
+    return navigateTo(localePath('/'))
+  }
+
+  return {
+    handleUserLogout
+  }
+}
