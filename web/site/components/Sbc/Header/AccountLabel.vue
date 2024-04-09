5b97520242d43b4f1de68e256116ead12079954a
deetz99 <dietrich@wolpert.ca>
2024-04-09 11:28:42 -0700

    add auth options in header component

---
+++ web/site/components/Sbc/Header/AccountLabel.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps({
+  accountName: { default: 'N/A', type: String },
+  username: { default: 'N/A', type: String }
+})
+</script>
+<template>
+  <div class="flex items-center">
+    <UAvatar
+      :alt="username[0]"
+      size="sm"
+      :ui="{
+        background: 'bg-bcGovBlue-300 dark:bg-[#E0E7ED]',
+        text: 'font-semibold leading-none text-white dark:text-bcGovColor-darkGray truncate',
+        placeholder: 'font-semibold leading-none text-white truncate dark:text-bcGovColor-darkGray text-xl',
+        rounded: 'rounded-sm'
+      }"
+    />
+    <div class="mx-2 flex flex-col text-left font-normal tracking-wide">
+      <span
+        class="line-clamp-1 font-semibold text-bcGovColor-darkGray dark:text-white"
+      >
+        {{ username.toUpperCase() }}
+      </span>
+      <span
+        class="line-clamp-1 text-bcGovColor-midGray dark:text-gray-300"
+      >
+        {{ accountName.toUpperCase() }}
+      </span>
+    </div>
+  </div>
+</template>
