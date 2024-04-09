7384b32afaaca34c4b24223a37b28d8c8d0d7426
deetz99 <dietrich@wolpert.ca>
2024-04-09 12:53:19 -0700

    remove signInWithRedirect option

--- web/site/pages/sbc/auth/login.vue
+++ web/site/pages/sbc/auth/login.vue
@@ -1,18 +1,13 @@
 <script lang="ts">
 // only load these once
 import {
-  getRedirectResult,
   OAuthProvider,
-  signInWithPopup,
-  signInWithRedirect,
-  setPersistence,
-  signOut,
-  browserSessionPersistence
+  signInWithPopup
+  // setPersistence,
+  // signOut,
+  // browserSessionPersistence,
+  // getAuth
 } from 'firebase/auth'
-import {
-  useCurrentUser,
-  useFirebaseAuth
-} from 'vuefire'
 
 export const bceidAuthProvider = new OAuthProvider('oidc.keycloak-bceid')
 export const bcscAuthProvider = new OAuthProvider('oidc.keycloak-bcsc')
@@ -21,52 +16,43 @@ export const idirAuthProvider = new OAuthProvider('oidc.keycloak-idir')
 
 <script setup lang="ts">
 const auth = useFirebaseAuth()! // only exists on client side
-const isLargeScreen = useMediaQuery('(min-width: 1024px)')
 const error = ref<Error | null>(null)
 
 useHead({
-  title: 'Service BC - Sign in'
+  title: 'Service BC - Log in'
 })
 const user = useCurrentUser()
 
-onMounted(() => {
-  getRedirectResult(auth).catch((reason) => {
-    console.error('Failed redirect result', reason)
+async function signIn (provider: OAuthProvider) {
+  error.value = null
+  signInWithPopup(auth, provider).catch((reason) => {
     error.value = reason
   })
-})
 
-function signIn (provider: OAuthProvider) {
-  if (isLargeScreen.value) {
-    signInWithPopup(auth, provider).catch((reason) => {
-      error.value = reason
-    })
-  } else {
-    signInWithRedirect(auth, provider).catch((reason) => {
-      error.value = reason
-    })
-  }
+  await navigateTo('/')
 }
 
 // const token = ref()
 // async function getToken () {
 //   token.value = await auth.currentUser?.getIdToken()
 // }
+watchEffect(() => console.log('error: ', error.value))
+watchEffect(() => console.log('user: ', user.value))
 </script>
 <template>
   <div class="p-4">
     <UCard class="max-w-md">
       <h1 class="text-2xl font-semibold text-bcGovColor-darkGray dark:text-white">
-        Sign in
+        Log in
       </h1>
       <p class="text-bcGovColor-midGray dark:text-gray-300">
-        Sign in to your Service BC account
+        Log in to your Service BC account
       </p>
       <img src="/img/BCReg_Generic_Login_image.jpg" class="my-4" alt="Generic login image">
       <div class="space-y-4">
-        <UButton label="Sign in with BC Services Card" block icon="i-mdi-card-account-details-outline" @click="signIn(bcscAuthProvider)" />
-        <UButton label="Sign in with BCeID" block icon="i-mdi-two-factor-authentication" color="gray" @click="signIn(bceidAuthProvider)" />
-        <UButton label="Sign in with IDIR" block icon="i-mdi-account-group-outline" color="gray" @click="signIn(idirAuthProvider)" />
+        <UButton label="Log in with BC Services Card" block icon="i-mdi-card-account-details-outline" @click="signIn(bcscAuthProvider)" />
+        <UButton label="Log in with BCeID" block icon="i-mdi-two-factor-authentication" color="gray" @click="signIn(bceidAuthProvider)" />
+        <UButton label="Log in with IDIR" block icon="i-mdi-account-group-outline" color="gray" @click="signIn(idirAuthProvider)" />
         <UDivider label="OR" />
         <UButton label="Create an Account" block color="gray" />
       </div>
