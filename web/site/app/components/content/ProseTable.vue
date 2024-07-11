e529774c99d29f9397c91c7539948cc54589d9b8
deetz99 <dietrich@wolpert.ca>
2024-07-10 17:39:39 -0700

    update prose table styling

--- web/site/app/components/content/ProseTable.vue
+++ web/site/app/components/content/ProseTable.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="w-full overflow-x-auto">
-    <table class="w-full min-w-[50rem] border-collapse overflow-x-hidden">
-      <slot />
-    </table>
+  <div class="w-full overflow-hidden rounded-lg border border-bcGovGray-100 dark:border-gray-300/50">
+    <div class="overflow-x-auto">
+      <table class="my-0 w-full min-w-[50rem]">
+        <slot />
+      </table>
+    </div>
   </div>
 </template>
---
+++ web/site/app/components/content/ProseTbody.vue
@@ -0,0 +1,5 @@
+<template>
+  <tbody class="divide-y divide-bcGovGray-100 dark:divide-gray-300/50">
+    <slot />
+  </tbody>
+</template>
