9e2ee83afa33d81a5fa6de7229b620229c162dd4
Rajandeep <rajandeep.kaur@gov.bc.ca>
2024-11-22 10:54:29 -0800

    Hiding Nav Items 'BN' 'CONNECT' Temporarily

--- web/site/app/utils/createContentNav.ts
+++ web/site/app/utils/createContentNav.ts
@@ -7,11 +7,15 @@ export function createContentNav (navItems: NavItem[] | undefined) {
   const localePath = useLocalePath()
 
   return navItems.flatMap((nav: NavItem) => {
-    return nav.children?.map((firstChild) => {
+    return nav.children?.filter((firstChild) => {
+      return !['connect', 'bn'].includes(firstChild.title.toLowerCase()) //filter bn , connect temp
+    }).map((firstChild) => {
       return { // create parent array for each UAccordian
         label: handleContentDirectoryLabel(firstChild.title), // return full string instead of 'bn', 'rs', etc
         defaultOpen: true, // accordians all open by default
-        children: firstChild.children?.map((secondChild) => { // create children array for each UVerticalNavigation
+        children: firstChild.children?.filter((secondChild) => {
+          return !['connect', 'bn'].includes(secondChild.title.toLowerCase()) // Exclude 'connect' or 'bn' at second level
+        }).map((secondChild) => { // create children array for each UVerticalNavigation
           return {
             label: secondChild.title,
             to: localePath(secondChild._path) // localize path
