79ed79bca44a7c2b4175fce45262848589bf477b
Rajandeep <rajandeep.kaur@gov.bc.ca>
2024-11-22 11:24:48 -0800

    filter api access request

--- web/site/app/utils/createContentNav.ts
+++ web/site/app/utils/createContentNav.ts
@@ -10,11 +10,12 @@ export function createContentNav (navItems: NavItem[] | undefined) {
     return nav.children?.filter((firstChild) => {
       return !['connect', 'bn'].includes(firstChild.title.toLowerCase()) // filter bn , connect temp
     }).map((firstChild) => {
+      const isGetStarted = firstChild.title.toLowerCase() === 'get started' || firstChild.title.toLowerCase() === 'get-started'
       return { // create parent array for each UAccordian
         label: handleContentDirectoryLabel(firstChild.title), // return full string instead of 'bn', 'rs', etc
         defaultOpen: true, // accordians all open by default
         children: firstChild.children?.filter((secondChild) => {
-          return !['connect', 'bn'].includes(secondChild.title.toLowerCase()) // Exclude 'connect' or 'bn' at second level
+          return !['connect', 'bn'].includes(secondChild.title.toLowerCase()) && (!isGetStarted || secondChild.title !== 'API Access Request') // Exclude 'connect' or 'bn' at second level
         }).map((secondChild) => { // create children array for each UVerticalNavigation
           return {
             label: secondChild.title,
