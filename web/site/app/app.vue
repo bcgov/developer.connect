1f0093de86d60ca5f3f14eb042851a9c6982093d
deetz99 <dietrich@wolpert.ca>
2025-02-11 11:29:43 -0800

    test theme

--- web/site/app/app.vue
+++ web/site/app/app.vue
@@ -93,3 +93,26 @@ provide('footerHeight', footerHeight)
     <SbcFooter ref="footerRef" class="z-50 mt-auto" />
   </div>
 </template>
+<style>
+  .light-mode {
+    --scalar-color-1: #f10dbc !important;
+    --scalar-color-2: rgba(0, 0, 0, 0.6);
+    --scalar-color-3: rgba(0, 0, 0, 0.4);
+    --scalar-color-accent: #0a85d1;
+    --scalar-background-1: #fff;
+    --scalar-background-2: #f6f5f4;
+    --scalar-background-3: #f1ede9;
+    --scalar-background-accent: #5369d20f;
+    --scalar-border-color: rgba(0, 0, 0, 0.08);
+  }
+  .dark-mode {
+    --scalar-color-1: rgba(255, 255, 255, 0.81);
+    --scalar-color-2: rgba(255, 255, 255, 0.443);
+    --scalar-color-3: rgba(255, 255, 255, 0.282);
+    --scalar-color-accent: #8ab4f8;
+    --scalar-background-1: #202020;
+    --scalar-background-2: #272727;
+    --scalar-background-3: #333333;
+    --scalar-background-accent: #8ab4f81f;
+  }
+</style>
